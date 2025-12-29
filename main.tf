37d14d98357eb30ce75a1c743bcc5a6f5a3bd00d
kushagra53 <kushagrapandey096@gmail.com>
2025-12-30 02:06:15 +0530

    fix: block public access for s3 bucket

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "demo" {
     Owner   = "kushagra"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
